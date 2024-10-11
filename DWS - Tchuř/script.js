68ae5a9a91eac2692aa2515d5f661fef5ed0e259
Zmar <159122092+SmetekTchur@users.noreply.github.com>
2024-10-11 20:13:42 +0200

    Update script.js

--- DWS - Tchuř/script.js
+++ DWS - Tchuř/script.js
@@ -1,6 +1,6 @@
 document.getElementById("moro").addEventListener("click", function() {
-    alert("NENÁVIDÍM JAVASCRIPT");
-  });
+alert("NENÁVIDÍM JAVASCRIPT");
+});
 
 
 function changeText() {
@@ -11,10 +11,50 @@ button.innerText = 'Text se změnil!';
 const button = document.getElementById('busy') ;
 const text = document.getElementById('auto');
 button.addEventListener('click',() => {
-        text.textContent = 'Smutné že';
-    })
+text.textContent = 'Smutné že';
+})
 
 function addText() {
 const container = document.getElementById('hub');
 container.innerHTML = '<p>Nenávídím JavaScript</p>';
+}
+
+document.getElementById('pozdravit').onclick = function() {
+const meno = document.getElementById('meno').value;
+alert('Ahoj '+ meno);
+};
+
+const textElement = document.getElementById('hoverText');
+
+textElement.addEventListener('mouseover', function() {
+textElement.innerText = 'Dík za najetí';
+});
+textElement.addEventListener('mouseout', function() {
+textElement.innerText = 'Najeď sem';
+
+});
+
+function zobrazCas() {
+var aktualniCas = new Date();
+var hodiny = aktualniCas.getHours();
+var minuty = aktualniCas.getMinutes();
+var sekundy = aktualniCas.getSeconds();
+
+if (hodiny < 10) hodiny = '0' + hodiny;
+if (minuty < 10) minuty = '0' + minuty;
+if (sekundy < 10) sekundy = '0' + sekundy;
+var cas = hodiny + ':' + minuty + ':' + sekundy;
+document.getElementById("cas").innerText = "Teď právě je: " + cas;
+}
+
+function zobrazDatum() {
+var aktualniDatum = new Date();
+var den = aktualniDatum.getDate();
+var mesic = aktualniDatum.getMonth() + 1;
+var rok = aktualniDatum.getFullYear();
+
+if (den < 10) den = '0' + den;
+if (mesic < 10) mesic = '0' + mesic;
+var datum = den + '.' + mesic + '.' + rok;
+document.getElementById("datum").innerText = "Co je za datum: " + datum;
 }
